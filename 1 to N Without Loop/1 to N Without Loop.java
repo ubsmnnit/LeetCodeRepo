893f9e1056ab106ae12b70f1e96c02c1e6b4ec08
Uday Bhan <158012869+ubsmnnit@users.noreply.github.com>
2024-02-07 00:21:38 +0530

    Update 1 to N Without Loop.java

--- 1 to N Without Loop/1 to N Without Loop.java
+++ 1 to N Without Loop/1 to N Without Loop.java
@@ -1,1 +1,27 @@
+public class Solution {
+    public static int[] printNos(int x) {
+        // Write Your Code Here
+       
+       int[] num_arr = new int[x];
+       pre_num(x,num_arr);
+       return num_arr;
 
+
+
+    }
+
+    public static int pre_num(int num, int[] arr){
+
+        if (num == 1) {
+            arr[0] = 1;
+        return 1; 
+        }
+        arr[num-1] = num;
+        pre_num(num-1,arr);
+
+        return 1;
+        
+    }
+
+    
+}
