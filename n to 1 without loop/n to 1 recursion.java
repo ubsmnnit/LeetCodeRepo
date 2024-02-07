a9416b036a83ffa6db0b677eed54f3e9f512c603
Uday Bhan <158012869+ubsmnnit@users.noreply.github.com>
2024-02-07 23:25:37 +0530

    Update n to 1 recursion.java

--- n to 1 without loop/n to 1 recursion.java
+++ n to 1 without loop/n to 1 recursion.java
@@ -1,1 +1,31 @@
+public class Solution
+{
+    public static int[] printNos(int x) {
+        // Write Your Code Here
+
+        int[] arr = new int[x];
+
+
+        next_num(x,x,arr);
+
+
+        return arr;
+
+    
+}
+    public static void next_num(int max, int n, int[] arr_){
+
+
+    if (n == 0) return ;
+
+
+    arr_[max-n] = n;
+
+    next_num(max,n-1,arr_);
+
+
+
+    }
+
+}
 
