068d7955fcdce699ec6fd399384fba2bacf21250
Uday Bhan <158012869+ubsmnnit@users.noreply.github.com>
2024-02-08 23:38:51 +0530

    Update reverse array.java

--- basic recursion/reverse array.java
+++ basic recursion/reverse array.java
@@ -1,1 +1,31 @@
 
+public class Solution {
+    public static int[] reverseArray(int n, int []nums) {
+        // Write your code here.
+
+        reverse(0, n, nums);
+
+        return nums;
+
+    }
+
+
+    public static void reverse(int i, int n, int [] nums){
+
+        if (i == n/2) return;
+
+        int temp = nums[i];
+
+        nums[i] = nums[n-1-i];
+        nums[n-1-i] = temp;
+
+        reverse(i + 1, n, nums);
+
+        return;
+        
+        
+
+    }
+
+    
+}
