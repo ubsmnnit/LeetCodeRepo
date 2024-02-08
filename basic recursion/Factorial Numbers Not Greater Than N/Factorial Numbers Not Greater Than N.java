3d4f07ba5fd66d03ccac5a8b692a337570ffb59f
Uday Bhan <158012869+ubsmnnit@users.noreply.github.com>
2024-02-08 23:40:46 +0530

    Update Factorial Numbers Not Greater Than N.java

--- basic recursion/Factorial Numbers Not Greater Than N/Factorial Numbers Not Greater Than N.java
+++ basic recursion/Factorial Numbers Not Greater Than N/Factorial Numbers Not Greater Than N.java
@@ -1,1 +1,27 @@
 
+import java.util.List;
+import java.util.ArrayList;
+public class Solution {
+    public static List<Long> factorialNumbers(long n) {
+        // Write Your Code Here
+
+        List<Long> fact_num = new ArrayList<>();
+
+        for( long i =1 ; fact(i) <= n; i++){
+
+            fact_num.add(fact(i));
+
+        }
+
+    return fact_num;
+
+    }
+
+public static long fact(long num){
+
+    if (num == 1) return 1; 
+
+    return num * fact(num -1);
+}
+
+}
