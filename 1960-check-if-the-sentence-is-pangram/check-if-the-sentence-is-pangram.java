5790c0995e65619820b6ba5b938d96a13e5e3054
Uday Bhan <158012869+ubsmnnit@users.noreply.github.com>
2024-02-19 01:21:54 +0530

    Time: 2 ms (70.25%) | Memory: 41.7 MB (37.36%) - LeetSync

--- 1960-check-if-the-sentence-is-pangram/check-if-the-sentence-is-pangram.java
+++ 1960-check-if-the-sentence-is-pangram/check-if-the-sentence-is-pangram.java
@@ -1,25 +1,19 @@
 class Solution {
     public boolean checkIfPangram(String sentence) {
 
-        Map<Character,Boolean> checker = new HashMap<>();
+        String alphbate = "qwertyuiopasdfghjklzxcvbnm" ;
         int count = 0;
 
-        for (int i = 0; i < sentence.length(); i++){
-            if(checker.containsKey(sentence.charAt(i))) {
-
-               // System.out.println(sentence.charAt(i));
-
-                checker.put(sentence.charAt(i),false);
-            }
-            else {
-                checker.put(sentence.charAt(i),true);
-            count += 1;
+        for (int i = 0; i < alphbate.length(); i++){
+           
+            if(sentence.contains(String.valueOf(alphbate.charAt(i)))){
+                count += 1;
 
             }
         }
-        //System.out.println(count);
+        System.out.println(count);
 
-       // System.out.println(sentence.length());
+       System.out.println(sentence.length());
 
         return count == 26 ;
         
